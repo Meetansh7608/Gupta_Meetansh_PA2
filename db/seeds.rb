05f6283adc3a6f7ba28d341fb0c125c181fe85e6
Meetansh <guptameetansh@gmail.com>
2023-09-25 17:55:15 -0500

    added more inital data movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@
 more_movies = [
   {:title => 'Lion King', :rating => 'G',
     :release_date => '24-Jun-1994',
-    :description => 'Amazing story, Great songs, Impeccable animation, what more do you need in life(Grade A in this course)!!!'}
+    :description => 'Amazing story, Great songs, Impeccable animation, what more do you need in life(Grade A in this course)!!!'},
   {:title => 'Murder Mystery', :rating => 'PG-13',
     :release_date => '14-Jun-2019',
     :description => 'Super cool mystery+comedy+action+thriller packed cinematic experience'}
