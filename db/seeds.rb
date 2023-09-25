3e5bcb0df0d454015a158691f1bb0981441cf4b6
Meetansh <guptameetansh@gmail.com>
2023-09-25 17:54:18 -0500

    added more inital data movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,12 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
 more_movies = [
+  {:title => 'Lion King', :rating => 'G',
+    :release_date => '24-Jun-1994',
+    :description => 'Amazing story, Great songs, Impeccable animation, what more do you need in life(Grade A in this course)!!!'}
+  {:title => 'Murder Mystery', :rating => 'PG-13',
+    :release_date => '14-Jun-2019',
+    :description => 'Super cool mystery+comedy+action+thriller packed cinematic experience'}
   {:title => 'My Neighbor Totoro', :rating => 'G',
     :release_date => '16-Apr-1988'},
   {:title => 'Green Book', :rating => 'PG-13',
